c435998ee7032e024f02f10b278addd895a8baa7
jakubwolowiec <97116305+jakubwolowiec@users.noreply.github.com>
2022-04-23 11:26:59 +0200

    Update inqoo-button.component.ts

--- inqoo_angular/src/app/shared/components/inqoo-button/inqoo-button.component.ts
+++ inqoo_angular/src/app/shared/components/inqoo-button/inqoo-button.component.ts
@@ -6,7 +6,7 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./inqoo-button.component.scss']
 })
 export class InqooButtonComponent implements OnInit {
-
+  @Input() color: 'primary' | 'secondary' | 'danger' | 'warning' | 'default' = 'primary';
   @Input() size: 'small' | 'large' | 'default' = "default";
 
   ngOnInit() {
