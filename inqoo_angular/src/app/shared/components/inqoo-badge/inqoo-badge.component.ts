299f18c92ca40e4a0e7aa9a7d8ead35f41bc916a
jakubwolowiec <97116305+jakubwolowiec@users.noreply.github.com>
2022-04-23 12:32:02 +0200

    adding counter

---
+++ inqoo_angular/src/app/shared/components/inqoo-badge/inqoo-badge.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-inqoo-badge',
+  templateUrl: './inqoo-badge.component.html',
+  styleUrls: ['./inqoo-badge.component.scss']
+})
+export class InqooBadgeComponent {
+
+  @Input() counter: number = 0;
+    
+  
+}
--- inqoo_angular/src/app/shared/components/inqoo-button/inqoo-button.component.ts
+++ inqoo_angular/src/app/shared/components/inqoo-button/inqoo-button.component.ts
@@ -1,17 +1,23 @@
-import {Component, Input, OnInit} from '@angular/core';
-
+import {Component, Input, Output, OnInit, EventEmitter} from '@angular/core';
+import {InqooButtonClickEvent} from "../../models/inqoo-button-click-event";
+import { InqooBadgeComponent } from '../inqoo-badge/inqoo-badge.component';
 @Component({
   selector: 'app-inqoo-button',
   templateUrl: './inqoo-button.component.html',
   styleUrls: ['./inqoo-button.component.scss']
 })
-export class InqooButtonComponent implements OnInit {
+export class InqooButtonComponent extends InqooBadgeComponent implements OnInit {
+
   @Input() color: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' = 'primary';
-  @Input() size: 'small' | 'large' | 'default' = "default";
+  @Input() size: 'small' | 'large' | 'default' = 'default';
+
+  @Output() buttonCustomClick = new EventEmitter<InqooButtonClickEvent>();
 
   ngOnInit() {
       console.log(this.size)
   }
+
+  onClick = () => this.counter =+1 ;
 
   isSmall = () => this.size === 'small';
   isLarge = () => this.size === 'large';
