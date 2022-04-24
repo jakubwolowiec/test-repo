6a2ff6e6953f7b6daac48ab2acb242a21611f541
jakubwolowiec <97116305+jakubwolowiec@users.noreply.github.com>
2022-04-24 10:27:50 +0200

    adding clock and navbar

---
+++ inqoo_angular/src/app/shared/components/inqoo-navbar/inqoo-navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inqoo-navbar',
+  templateUrl: './inqoo-navbar.component.html',
+  styleUrls: ['./inqoo-navbar.component.scss']
+})
+export class InqooNavbarComponent implements OnInit {
+
+  @Input() routes: {label: string, }[] =[];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
