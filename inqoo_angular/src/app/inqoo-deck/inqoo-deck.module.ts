f769df98e3380e58e426f121b3f0bd7025616407
jakubwolowiec <97116305+jakubwolowiec@users.noreply.github.com>
2022-05-06 14:29:24 +0200

    restoring repo

---
+++ inqoo_angular/src/app/inqoo-deck/inqoo-deck.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InqooDeckComponent } from './pages/inqoo-deck/inqoo-deck.component';
+
+
+
+@NgModule({
+  declarations: [
+    InqooDeckComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class InqooDeckModule { }
---
+++ inqoo_angular/src/app/inqoo-deck/pages/inqoo-deck/inqoo-deck.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inqoo-deck',
+  templateUrl: './inqoo-deck.component.html',
+  styleUrls: ['./inqoo-deck.component.scss']
+})
+export class InqooDeckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
