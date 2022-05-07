5bb71596c1d036b81cafed5c117c056dccb04d85
jakubwolowiec <97116305+jakubwolowiec@users.noreply.github.com>
2022-05-07 15:51:33 +0200

    added searchbar

--- inqoo_angular/src/app/sw-starships/components/starships-list/starships-list.component.ts
+++ inqoo_angular/src/app/sw-starships/components/starships-list/starships-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Starship} from "../../../api/models/starship";
 import {SwapiService} from "../../../api/services/swapi.service";
 import {delay, map} from "rxjs";
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-starships-list',
@@ -9,26 +10,31 @@ import {delay, map} from "rxjs";
   styleUrls: ['./starships-list.component.scss']
 })
 export class StarshipsListComponent implements OnInit {
-
-  starships: Starship[] = [];
   starshipsBase: Starship[] = [];
+  starships: Starship[] = [];
   isLoading: boolean = true;
 
   constructor(private swapi: SwapiService) { }
 
   ngOnInit(): void {
+
     this.swapi.getStarships().pipe(
       delay(1000),
       map(data => data.results as Starship[])
     ).subscribe(result => {
       this.starships = result;
-      this.starshipsBase = result;
+      this.starshipsBase = this.starships;
       this.isLoading = false;
     });
   }
 
-  onSearchChange = (searchTerm: string) =>
-    searchTerm === '' ?
-      this.starships = [...this.starshipsBase]
-      : this.starships = this.starshipsBase.filter(ship => ship.name.includes(searchTerm));
-}
+  onSearchChange = (searchTerm: string) => {
+    if(searchTerm != null){
+      searchTerm = searchTerm.toLowerCase();
+    
+    this.starships = this.starshipsBase.filter(ship => ship.name.includes(searchTerm))
+    }
+    else{this.starships = [...this.starshipsBase];}
+  }
+  }
+
